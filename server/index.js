a4980b6d75f6659b0539154191a914a7805ec5c9
Rishabh Singh <mrrobot@Rishabhs-MacBook-Air.local>
2023-06-26 18:27:41 +0530

    auth completed

--- server/index.js
+++ server/index.js
@@ -8,12 +8,13 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
 const {fileURLToPath} = require("url");
+const authRoutes = require("./routes/auth");
 const {register} = require("./controllers/auth");
 
 // Configuration 
 dotenv.config();
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(morgan("common"));
 app.use(BodyParser.json({limit:"30mb",extended:true}));
@@ -34,8 +35,10 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 //routes with files
-app.post("/aut/register",upload.single("picture"),register);
+app.post("/auth/register",upload.single("picture"),register);
 
+//routes
+app.use("/auth",authRoutes);
 //mongoose
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URI,{
