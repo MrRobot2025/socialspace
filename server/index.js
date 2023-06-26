93377ff762facef9005c54445b689adfe23ac9e3
Rishabh Singh <mrrobot@Rishabhs-MacBook-Air.local>
2023-06-26 15:49:23 +0530

    created index.js auth.js - added all dependencies

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const BodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const multer = require("multer");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+const {fileURLToPath} = require("url");
+const {register} = require("./controllers/auth");
+
+// Configuration 
+dotenv.config();
+const app = express();
+app.use(express.json);
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(BodyParser.json({limit:"30mb",extended:true}));
+app.use(BodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+
+
+//File storage
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"public/assets");
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname);
+    }
+});
+const upload = multer({storage});
+
+//routes with files
+app.post("/aut/register",upload.single("picture"),register);
+
+//mongoose
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    app.listen(PORT,()=>console.log(`Server PORT: ${PORT}`))
+}).catch((err)=> console.log(`Did NOT connect ${err}`));
